9a852a38333369aecb146a7b1bd9e9fbf448e100
Mamdouh Abdulhaleem <mrma11581@gmail.com>
2020-04-25 21:49:00 +0300

    chips

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ new Vue({
     data: {
         search:'',
         show:false,
-        m1: "You got to let me know",
+        m1: "Chips Dropdowen Vue js",
         list:[
-            {id:0,sl:false,name:'Egypt'},
-            {id:1,sl:false,name:'Kuwait'},
-            {id:2,sl:false,name:'Jordn'},
-            {id:3,sl:false,name:'Tuniis'},
-            {id:4,sl:false,name:'Moroco'},
-            {id:5,sl:false,name:'United arabic'},
-            {id:6,sl:false,name:'Saudia'},
-            {id:7,sl:false,name:'syria'},
-            {id:8,sl:false,name:'Yeaman'}
+            {id:0,sl:false,name:'Egypt',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/100px-Flag_of_Egypt.svg.png'},
+            {id:1,sl:false,name:'Kuwait',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Flag_of_Kuwait.svg/100px-Flag_of_Kuwait.svg.png'},
+            {id:2,sl:false,name:'Jordan',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Flag_of_Jordan.svg/100px-Flag_of_Jordan.svg.png'},
+            {id:3,sl:false,name:'Tunisia',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Tunisia.svg/100px-Flag_of_Tunisia.svg.png'},
+            {id:4,sl:false,name:'Morocco',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Morocco.svg/100px-Flag_of_Morocco.svg.png'},
+            {id:5,sl:false,name:'United Arab Emirates',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_United_Arab_Emirates.svg/100px-Flag_of_the_United_Arab_Emirates.svg.png'},
+            {id:6,sl:false,name:'Saudi Arabia',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Flag_of_Saudi_Arabia.svg/100px-Flag_of_Saudi_Arabia.svg.png'},
+            {id:7,sl:false,name:'Syria',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Flag_of_Syria.svg/100px-Flag_of_Syria.svg.png'},
+            {id:8,sl:false,name:'Yemen',img:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Flag_of_Yemen.svg/100px-Flag_of_Yemen.svg.png'}
     
     
     
